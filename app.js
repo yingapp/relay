8547949cb7f2d9202a0f512e5f46630e7fc21f18
LY <dsgderer5g+yingapp@users.noreply.github.com>
2020-09-15 18:02:13 +0800

    make it better

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const WebTorrent = require('webtorrent-hybrid')
 require('events').EventEmitter.defaultMaxListeners = 250
 const fs = require('fs')
-const client = new WebTorrent()
+var SimplePeer = require('simple-peer')
+const WebTorrent = require('webtorrent')
+const client = new WebTorrent({
+    tracker: {
+        rtcConfig: {
+            ...SimplePeer.config,
+            ...[
+                {
+                    urls: [
+                        'stun:stun.l.google.com:19302',
+                        'stun:global.stun.twilio.com:3478'
+                    ]
+                },
+            ]
+        }
+    }
+})
+// const client = new WebTorrent()
 var express = require('express');
 var app = express();
 const cache = {}
@@ -9,17 +25,35 @@ app.get('/:magnet', function (req, res) {
     const url = req.url;
     if (url.substr(0, 8) === '/magnet:') {
         const torrentId = url.substr(1)
-        if (!cache[torrentId]) {
+        if (cache[torrentId]) {
+            console.log('已在下载：', torrentId)
+        } else {
             cache[torrentId] = true
             console.log('torrentId:\t', torrentId)
             client.add(torrentId, { path: __dirname + '/files' })
+
+            // client.add(torrentId, torrent => {
+            //     const files = torrent.files
+            //     let length = files.length
+            //     // Stream each file to the disk
+            //     files.forEach(file => {
+            //         const source = file.createReadStream()
+            //         const destination = fs.createWriteStream(__dirname + '/files/' + file.name)
+            //         source.on('end', () => {
+            //             console.log('file:\t\t', file.name)
+            //             // close after all files are saved
+            //             length -= 1
+            //             if (!length) process.exit()
+            //         }).pipe(destination)
+            //     })
+            // })
         }
     }
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.writeHead(200, { 'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*' });
     res.end(url);
 });
 var server = require('http').Server(app);
 var port = process.env.PORT || 60;
 server.listen(port, function () {
-  console.log('Server listening at port %d', port);
+    console.log('Server listening at port %d', port);
 });
